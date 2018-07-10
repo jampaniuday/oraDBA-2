92421e11c0b0c431563cce83cd5eeea65a9a5cd6
wengyan <yan.weng@outlook.com>
2018-07-10 17:18:02 +0800

    2018-07-10

--- instance.sql
+++ instance.sql
@@ -1,3 +1,9 @@
 
 -- check instance name and status
 SELECT INSTANCE_NAME, STATUS, DATABASE_STATUS FROM V$INSTANCE;
+
+-- show service name
+show parameter service_names
+
+-- show instance name
+show parameter instance_name
