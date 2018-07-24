477eb834eaad60d8efa963a712a9cb892336f996
wengyan <yan.weng@outlook.com>
2018-07-24 08:23:51 +0800

    2018-07-24

--- block.sql
+++ block.sql
@@ -1,2 +1,8 @@
 -- returns the database block number for the input ROWID
 select DBMS_ROWID.ROWID_BLOCK_NUMBER(rowid) from t;
+
+-- show extent/block of a table
+select extent_id, bytes, blocks
+from user_extents
+where segment_name = 'T'
+order by extent_id;
