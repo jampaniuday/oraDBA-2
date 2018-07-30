132251ab4241fdea5d0ebaeea1a76199fb81a095
wengyan <yan.weng@outlook.com>
2018-07-30 08:34:53 +0800

    2018-07-30

--- redo_arch.sql
+++ redo_arch.sql
@@ -52,3 +52,30 @@ select * from V$LOGFILE;
 
 -- information about archived logs that are needed to complete media recovery
 select * from V$RECOVERY_LOG;
+
+
+
+create or replace function get_stat_val( p_name in varchar2) return number
+as
+    l_val number;
+begin
+    select b.value
+    into l_val
+    from v$statname a, v$mystat b
+    where a.statistic# = b.statistic#
+    and a.name = p_name;
+    
+    return l_val;
+end;
+/
+
+
+
+
+
+
+
+
+
+
+
--- undo.sql
+++ undo.sql
@@ -21,3 +21,22 @@ AUTOEXTEND ON NEXT 8192 MAXSIZE 32767M;
 
 -- Modify the database parameter to use the new UNDO tablespace.
 ALTER SYSTEM SET undo_tablespace=UNDO2 SCOPE=BOTH;
+
+
+-- check transaction used undo bulk
+update t set indexed = lower(indexed);
+select used_ublk
+   from v$transaction
+   where addr = (select taddr
+                 from v$session
+                 where sid = (select sid
+                              from v$mystat
+                              where rownum = 1
+                             )
+                )
+/
+
+
+select * from v$undostat;
+
+show parameter undo;
